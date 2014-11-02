86627673f1a7384b5b6a48c7bfbe11eba47f7105
samrooney <sam_rooney@asl.org>
2014-11-02 20:44:31 +0000

    FINISHED

--- Lab03.java
+++ Lab03.java
@@ -2,8 +2,8 @@
 *
 * Description of the program goes here  // provide a brief description
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author <SAM ROONEY>  // replace <SAM> with your name
+* @version <date/of/completion> // replace <2/11.14> with the date
 */
 
 import edu.fcps.karel2.Display;
@@ -12,6 +12,52 @@ import edu.fcps.karel2.Robot;
 public class Lab03 {
 
      public static void main(String[] args) {
-          // TODO Your code goes here
-     }
+            
+            Display.openWorld("maps/mountain.map");
+            Display.setSize(16, 16);
+            Display.setSpeed(5);
+            
+               Climber BRUV = new Climber();
+               Climber FAM = new Climber();
+            
+            BRUV.turnRight();
+               FAM.turnRight();
+            BRUV.move();
+               FAM.move();
+            BRUV.turnLeft();
+               FAM.turnLeft();
+            BRUV.climbUpRight();
+               FAM.climbUpRight();
+            BRUV.climbUpRight();
+               FAM.climbUpRight();
+            BRUV.climbUpRight();
+               FAM.climbUpRight();
+            BRUV.turnAround();
+               FAM.turnAround();
+            BRUV.climbDownRight();
+               FAM.climbDownRight();
+            BRUV.climbDownRight();
+               FAM.climbDownRight();
+            BRUV.pickBeeper();
+               FAM.turnAround();
+            BRUV.turnAround();
+               FAM.climbUpLeft();
+            BRUV.climbUpLeft();
+               FAM.climbUpLeft();
+            BRUV.climbUpLeft();
+               FAM.turnAround();
+            BRUV.turnAround();
+               FAM.climbDownLeft();
+            BRUV.climbDownLeft();
+               FAM.climbDownLeft();
+            BRUV.climbDownLeft();
+               FAM.climbDownLeft();
+            BRUV.climbDownLeft();
+               FAM.turnRight();
+            BRUV.turnRight();
+               FAM.move();
+            BRUV.move();
+               FAM.turnRight();
+            BRUV.turnRight();          
+     }   
 }
